b056e744c0dc3a74a115c6af779aced0b9f3ea85
Frenco <hey@frenco.dev>
2020-12-03 12:08:52 +0630

    feat: migrate to null-safety

--- lib/typeweight.dart
+++ lib/typeweight.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
 abstract class TypeWeight {
-  static final FontWeight thin = FontWeight.w100;
-  static final FontWeight extraLight = FontWeight.w200;
-  static final FontWeight light = FontWeight.w300;
-  static final FontWeight regular = FontWeight.w400;
-  static final FontWeight medium = FontWeight.w500;
-  static final FontWeight semiBold = FontWeight.w600;
-  static final FontWeight bold = FontWeight.w700;
-  static final FontWeight extraBold = FontWeight.w800;
-  static final FontWeight black = FontWeight.w900;
+  /// Equivalent to `FontWeight.w100`
+  static const FontWeight thin = FontWeight.w100;
+
+  /// Equivalent to `FontWeight.w200`
+  static const FontWeight extraLight = FontWeight.w200;
+
+  /// Equivalent to `FontWeight.w300`
+  static const FontWeight light = FontWeight.w300;
+
+  /// Equivalent to `FontWeight.w400`
+  static const FontWeight regular = FontWeight.w400;
+
+  /// Equivalent to `FontWeight.w500`
+  static const FontWeight medium = FontWeight.w500;
+
+  /// Equivalent to `FontWeight.w600`
+  static const FontWeight semiBold = FontWeight.w600;
+
+  /// Equivalent to `FontWeight.w700`
+  static const FontWeight bold = FontWeight.w700;
+
+  /// Equivalent to `FontWeight.w800`
+  static const FontWeight extraBold = FontWeight.w800;
+
+  /// Equivalent to `FontWeight.w900`
+  static const FontWeight black = FontWeight.w900;
 }
