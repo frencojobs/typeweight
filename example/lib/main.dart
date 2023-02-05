c77142ff679809990e6f130afa55042453db2093
Frenco <hey@frenco.dev>
2023-02-05 12:53:31 +0630

    feat: update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:typeweight/typeweight.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'TypeWeight Demo',
       home: Scaffold(
         body: Center(
